fd3210bf286031505ae121c8f3075d4dba1ef289
hyori100 <55873546+hyori100@users.noreply.github.com>
2023-12-21 23:14:24 +0900

    [level 1] Title: 음양 더하기, Time: 0.12 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/1/76501. 음양 더하기/음양 더하기.py
+++ 프로그래머스/1/76501. 음양 더하기/음양 더하기.py
@@ -1,6 +1,6 @@
 def solution(absolutes, signs):
     answer = []
-    for absolute,sign in zip(absolutes, signs):
+    for absolute,sign in zip(absolutes, signs): #zip - 다중 리스트를 순서대로 요소를 뽑아줌
         if sign == False:
             answer.append(absolute * -1)
         else:
