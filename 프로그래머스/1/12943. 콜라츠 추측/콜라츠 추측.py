c6d8de9a537f9b258b9577fe5762ceca0e63bc79
hyori100 <55873546+hyori100@users.noreply.github.com>
2023-12-21 23:25:54 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.04 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.py
+++ 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.py
@@ -3,13 +3,12 @@ def solution(num):
     if num == 1:
         return 0
 
-    while num > 1:
+    while num != 1:
         if count == 500:
-            count = -1
-            break
+            return -1
         if num % 2 == 0:
             num = num/2
         else :
             num = num*3 + 1
         count += 1
-    return count 
+    return count    
