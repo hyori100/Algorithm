41b3797ef5f8d56f3851ca69bf06420ef0b9e854
hyori100 <55873546+hyori100@users.noreply.github.com>
2023-12-30 22:21:32 +0900

    [level 1] Title: 크기가 작은 부분문자열, Time: 0.05 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/1/147355. 크기가 작은 부분문자열/크기가 작은 부분문자열.py
+++ 프로그래머스/1/147355. 크기가 작은 부분문자열/크기가 작은 부분문자열.py
@@ -1,10 +1,8 @@
 def solution(t, p):
     answer = 0
-    nums = []
-    pLen = len(p)
     listT = list(t)
     for index, value in enumerate(listT):
-        if index + pLen < len(listT)+1 and int("".join(listT[index:index+pLen])) <= int(p):
+        if index + len(p) < len(listT)+1 and int("".join(listT[index:index+len(p)])) <= int(p):
             answer += 1
     return answer
             
