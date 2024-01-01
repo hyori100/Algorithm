74a1b8066a312f80925e765ef4e13b45117495b1
hyori100 <55873546+hyori100@users.noreply.github.com>
2024-01-01 21:57:53 +0900

    [level 1] Title: 숫자 문자열과 영단어, Time: 0.03 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.py
@@ -0,0 +1,8 @@
+def solution(s):
+    answer = s
+    dic = {0: 'zero', 1: 'one', 2: 'two', 3: 'three', 4: 'four', 5: 'five', 6: 'six', 7: 'seven', 8: 'eight', 9: 'nine'}
+    
+    for key, value in dic.items():
+        if value in s:
+            answer = answer.replace(value, str(key))
+    return int(answer)
