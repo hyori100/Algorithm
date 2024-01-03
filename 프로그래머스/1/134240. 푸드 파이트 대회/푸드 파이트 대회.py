37b43bf67c8bf3d2fa37800c12db43294e390fe7
hyori100 <55873546+hyori100@users.noreply.github.com>
2024-01-03 21:45:22 +0900

    [level 1] Title: 푸드 파이트 대회, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/1/134240. 푸드 파이트 대회/푸드 파이트 대회.py
+++ 프로그래머스/1/134240. 푸드 파이트 대회/푸드 파이트 대회.py
@@ -1,7 +1,6 @@
 def solution(food):
     answer = ''
     for index, value in enumerate(food[1:]):
-        if value//2 > 0:
-            answer += str(index + 1) * (value//2)
+        answer += str(index + 1) * (value//2)
     answer += '0' + answer[::-1]
     return answer
