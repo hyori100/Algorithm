33a4e971aaef275406514de0a5d69427022d3468
hyori100 <55873546+hyori100@users.noreply.github.com>
2024-01-07 16:29:37 +0900

    [level 1] Title: 카드 뭉치, Time: 0.02 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/1/159994. 카드 뭉치/카드 뭉치.py
+++ 프로그래머스/1/159994. 카드 뭉치/카드 뭉치.py
@@ -1,12 +1,13 @@
 def solution(cards1, cards2, goal):
-    dic1 = dict([(cards1[i], i) for i in range(len(cards1))])
-    dic2 = dict([(cards2[i], i) for i in range(len(cards2))])
-    Alist = []
-    Blist = []
-    for word in goal:
-        if word in dic1:
-            Alist.append(dic1[word])
-        else:
-            Blist.append(dic2[word])
+    dic = {}
+    for index, card in enumerate(cards1):
+        dic[card] = ('A', index)
+    for index, card in enumerate(cards2):
+        dic[card] = ('B', index)
+    for index, word in enumerate(goal):
+        goal[index] = dic[word]
+        
+    Alist = [y for x,y in goal if x == "A"]
+    Blist = [y for x,y in goal if x == "B"]
 
     return "Yes" if all(x<y and x+1==y for x, y in zip(Alist[:-1], Alist[1:])) and all(x<y and x+1==y for x, y in zip(Blist[:-1], Blist[1:])) else "No"
