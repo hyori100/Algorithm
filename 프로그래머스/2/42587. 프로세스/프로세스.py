d1530c26a18a7d759f50239dfaea987674c56c7a
마효리 Ma Hyo Ri <55873546+hyori100@users.noreply.github.com>
2025-04-12 11:08:30 +0900

    [level 2] Title: 프로세스, Time: 1.58 ms, Memory: 9.28 MB -BaekjoonHub

---
+++ 프로그래머스/2/42587. 프로세스/프로세스.py
@@ -0,0 +1,18 @@
+from collections import deque
+
+def solution(priorities, location):
+    q = deque()
+    sequence = 0
+    for idx, p in enumerate(priorities):
+        q.append((idx, p))
+    while q:
+        maxNum = max([b for a,b in q])
+        idx, item = q.popleft()
+        if item < maxNum:
+            q.append((idx,item))
+        else:
+            sequence += 1
+            if idx == location:
+                return sequence
+        
+    
