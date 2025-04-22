327e5fe838630293604a6a793b79ee39aa50694c
마효리 Ma Hyo Ri <55873546+hyori100@users.noreply.github.com>
2025-04-22 23:01:52 +0900

    [Silver IV] Title: 스택, Time: 44 ms, Memory: 33432 KB -BaekjoonHub

--- 백준/Silver/10828. 스택/스택.py
+++ 백준/Silver/10828. 스택/스택.py
@@ -2,23 +2,20 @@ import sys
 
 N = int(sys.stdin.readline())
 stack = []
-cnt = 0
 
 for _ in range(N):
     s = sys.stdin.readline().split()
     if s[0] == "push":
         num = int(s[1])
         stack.append(num)
-        cnt += 1
     elif s[0] == "pop":
         if stack:
             num = stack.pop()
-            cnt -= 1
             print(num)
         else:
             print(-1)
     elif s[0] == "size":
-        print(cnt)
+        print(len(stack))
     elif s[0] == "empty":
         print(0 if stack else 1)
     elif s[0] == "top":
