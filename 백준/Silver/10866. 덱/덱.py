b68929ad037c0b3e0ce95ee602d89d24654de06c
마효리 Ma Hyo Ri <55873546+hyori100@users.noreply.github.com>
2025-04-24 22:30:37 +0900

    [Silver IV] Title: 덱, Time: 64 ms, Memory: 34952 KB -BaekjoonHub

---
+++ 백준/Silver/10866. 덱/덱.py
@@ -0,0 +1,24 @@
+import sys
+from collections import deque
+
+N = int(sys.stdin.readline())
+d = deque()
+
+for i in range(0,N):
+    s = sys.stdin.readline().split()
+    if s[0] == "push_front":
+        d.appendleft(s[1])
+    elif s[0] == "push_back":
+        d.append(s[1])
+    elif s[0] == "pop_front":
+        print(d.popleft() if d else -1)
+    elif s[0] == "pop_back":
+        print(d.pop() if d else -1)
+    elif s[0] == "size":
+        print(len(d))
+    elif s[0] == "empty":
+        print(0 if d else 1)
+    elif s[0] == "front":
+        print(d[0] if d else -1)
+    elif s[0] == "back":
+        print(d[-1] if d else -1)
