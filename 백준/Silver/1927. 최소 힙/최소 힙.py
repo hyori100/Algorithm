c0bfc2e2e87f91e6b94698611331b7ef0d805ee4
마효리 Ma Hyo Ri <55873546+hyori100@users.noreply.github.com>
2025-04-29 22:18:29 +0900

    [Silver II] Title: 최소 힙, Time: 112 ms, Memory: 38340 KB -BaekjoonHub

---
+++ 백준/Silver/1927. 최소 힙/최소 힙.py
@@ -0,0 +1,12 @@
+import sys
+import heapq
+        
+N = int(sys.stdin.readline())
+heap = []
+
+for i in range(N):
+    M = int(sys.stdin.readline())
+    if M == 0:
+        print(heapq.heappop(heap) if heap else 0)
+    else:
+        heapq.heappush(heap, M)
