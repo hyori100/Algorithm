f66c5d22e788cac8fc966971f4e255df17d05428
마효리 Ma Hyo Ri <55873546+hyori100@users.noreply.github.com>
2025-05-20 23:30:40 +0900

    [Bronze I] Title: 수 정렬하기 3, Time: 8704 ms, Memory: 33432 KB -BaekjoonHub

---
+++ 백준/Bronze/10989. 수 정렬하기 3/수 정렬하기 3.py
@@ -0,0 +1,14 @@
+import sys
+
+N = int(sys.stdin.readline().strip())
+
+count = [0] * 10001
+
+for _ in range(N):
+    num = int(sys.stdin.readline())
+    count[num] += 1
+
+for i in range(1, 10001):
+    if count[i] != 0:
+        for _ in range(count[i]):
+            print(i)
