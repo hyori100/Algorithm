7e234dbc682076a1bface49ab155f5090204573b
마효리 Ma Hyo Ri <55873546+hyori100@users.noreply.github.com>
2025-05-28 00:12:59 +0900

    [level 2] Title: 조건에 맞는 도서와 저자 리스트 출력하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/2/144854. 조건에 맞는 도서와 저자 리스트 출력하기/조건에 맞는 도서와 저자 리스트 출력하기.sql
@@ -0,0 +1,7 @@
+-- 코드를 입력하세요
+SELECT BOOK_ID, AUTHOR_NAME, date_format(PUBLISHED_DATE, '%Y-%m-%d') as PUBLISHED_DATE
+from BOOK as a
+join AUTHOR as b 
+on a.AUTHOR_ID = b.AUTHOR_ID
+where a.CATEGORY = '경제'
+order by PUBLISHED_DATE;
